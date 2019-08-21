3e2c6e34412b56fdd70de6ddb0e2ded9550a17b6
Shea Polansky <spolansky@securityevaluators.com>
2019-08-21 15:01:06 -0700

    Iniitial commit, Version 1.0

---
+++ src/main/resources/archetype-resources/src/main/java/burp/BurpExtender.java
@@ -0,0 +1,30 @@
+//Hello World burp extension taken from https://github.com/PortSwigger/example-hello-world/tree/master/java
+package burp;
+
+import java.io.PrintWriter;
+
+public class BurpExtender implements IBurpExtender
+{
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
+    {
+        // set our extension name
+        callbacks.setExtensionName("Hello world extension");
+        
+        // obtain our output and error streams
+        PrintWriter stdout = new PrintWriter(callbacks.getStdout(), true);
+        PrintWriter stderr = new PrintWriter(callbacks.getStderr(), true);
+        
+        // write a message to our output stream
+        stdout.println("Hello output");
+        
+        // write a message to our error stream
+        stderr.println("Hello errors");
+        
+        // write a message to the Burp alerts tab
+        callbacks.issueAlert("Hello alerts");
+        
+        // throw an exception that will appear in our error stream
+        throw new RuntimeException("Hello exceptions");
+    }
+}
